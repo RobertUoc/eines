eb7de051e5b91e91906ed00f3029a0b369395652
Robert Artesona Torres <rartesona@uoc.edu>
2024-10-24 19:55:10 +0200

    arreglos2

---
+++ dist/index.15d17b1c.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("a").forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),setTimeout(()=>{window.location=this.href},500)})});
+//# sourceMappingURL=index.15d17b1c.js.map
---
+++ dist/index.ed6c40e3.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("a").forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),setTimeout(()=>{window.location=this.href},500)})});
+//# sourceMappingURL=index.ed6c40e3.js.map
