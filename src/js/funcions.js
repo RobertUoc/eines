f03b6dbfb09f08ea94d00985249cc2945f9fe92c
Robert Artesona Torrs <rartesona@uoc.edu>
2025-01-07 09:15:24 +0100

    finish

--- src/js/funcions.js
+++ src/js/funcions.js
@@ -21,8 +21,8 @@ async function principal() {
     img.setAttribute('alt', 'llieda');    
     img.setAttribute('rel', 'preload');    
     img.setAttribute('as', 'image');  
-    img.setAttribute('width', '427px');  
-    img.setAttribute('height', '261px');  
+    img.setAttribute('width', '364px');  
+    img.setAttribute('height', '237px');  
     img.setAttribute('loading', 'lazy');  
     img.classList.add('fotografia');    
     img.classList.add('clip-animation');        
